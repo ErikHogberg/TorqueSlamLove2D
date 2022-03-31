c894de043bd6ea7f3bfbfa6e6d675458f4f373ee
Erik Högberg <erikh_93@hotmail.com>
2022-03-31 18:28:15 +0200

    added comments

--- main.lua
+++ main.lua
@@ -3,27 +3,34 @@
 players = {}
 playerCount = 2
 
+-- player input keys
 keys = {
-    {'w','s','a','d','e','q'},
+    {'w','s','a','d','e','q'}, -- up, down, left, right, clockwise, counter-clockwise
     {'i','k','j','l','o','u'},
 }
 
 for i=1,playerCount do
     players[i] = {
+
+        -- position
         x=100 * i,
         y=100,
+        rot = 0,
     
+        -- velocity
         vX = 0,
         vY = 0,
         torque = 0,
         
+        -- input buffers
         xDir = 0,
         yDir = 0,
         lastDir = 1,
     
-        rot = 0,
+        -- if ground was touched last frame
         touchingGround = false,
 
+        -- assign input keys
         upKey = keys[i][1],
         downKey = keys[i][2],
         leftKey = keys[i][3],
@@ -33,37 +40,38 @@ for i=1,playerCount do
     }
 end
 
-
+-- shared player settings
 accel = 200
 rollStrength = 50
 rollImpulseStrength = 10
-
-gravity = 9.82 * 10
-downFactor = 20
-drag = 100
-aDrag = 20
-airTorqueDrag = 1
-torqueTransfer = 30
-
 jumpForce = 150
 
-vCap = 100
+-- physics settings
+gravity = 9.82 * 10
+downFactor = 20 -- How much torque causes player to be pushed downwards, multiplier
+drag = 100 -- movement slowdown while touching ground
+aDrag = 20 -- rotation slowdown while touching ground
+airTorqueDrag = 1-- rotation slowdown while in-air
+torqueTransfer = 30 -- the convertion rate of rotation to movement while touching ground
+vCap = 100 -- velocity cap
 
-size = 30
-rectW = 15
-rectH = 45
-groundHeight = 10
+-- graphics settings
+size = 30 -- radius of ball, is alos physics size
+rectW = 15 -- spinning rectangle width
+rectH = 45 -- spinning rectangle width
+groundHeight = 10 -- starting height of ground pillars
 
-turnLoss = .5
-launchMul = 1
-torqueToHeight = .5
-velocityToHeight = .1
+turnLoss = .5 -- how much % of X velocity is kept on bouncing off walls
+launchMul = 1 -- convertion rate of pillar height displacement to Y velocity upon getting pushed up by a rising pillar
+torqueToHeight = .5 -- convertion rate of torque to pillar height increase on landing
+velocityToHeight = .1 -- convertion rate of Y velocity to pillar height increase on landing
 
-pillarCount = 6
+pillarCount = 6 -- how many pillars the ground is subdivided into
 pillars = {}
 
-debugval =1
+debugval = 1
 
+-- initialize pillar heights to 0 (rendered with floor height added)
 for i = 0, pillarCount +1 do pillars[i] = 0 end
 
 function sign(number)
@@ -74,35 +82,47 @@ function love.update(dt)
 
     -- TODO: player-to-player collision
 
+    -- player update loop
     for i=1,playerCount do
 
+        -- apply gravity
         players[i].vY = players[i].vY + gravity * dt
 
-        -- players[i].vX = players[i].vX + players[i].xDir * accel * dt
         if players[i].vY > 0 then
+            -- push player downwards depending on torque compared to movement direction, cw for right, ccw for left
             if players[i].vX > 0 then
                 players[i].vY = players[i].vY + math.max(0, players[i].torque) * downFactor * dt
             elseif players[i].vX < 0 then
                 players[i].vY = players[i].vY + math.min(0, players[i].torque) * downFactor * dt
             end
         end
+
+        -- add torque to rotation
         players[i].rot = players[i].rot + players[i].torque * dt
-        -- torque = torque - aDrag * dt
+
+        -- calculate potential future position
         newX = players[i].x + players[i].vX * dt
         newY = players[i].y + players[i].vY * dt
 
-        
         screenWidth = love.graphics.getWidth()
+
         pillarWidth = screenWidth/pillarCount
+        
+        -- get pillar currently below player
         pillarIndex = math.floor(players[i].x/(pillarWidth))+1
+        -- get pillar below future player position
         newPillarIndex = math.floor((newX + size * sign(newX - players[i].x ))/(pillarWidth))+1
+        -- get height of future pillar
         newPillarHeight = love.graphics.getHeight() - groundHeight -pillars[newPillarIndex]
 
         if newY - size > 0 then
-            if newY + size < love.graphics.getHeight() - groundHeight - pillars[pillarIndex] then    
+            if newY + size < love.graphics.getHeight() - groundHeight - pillars[pillarIndex] then
+                -- if in-air
                 players[i].touchingGround = false
                 players[i].y = newY
             else 
+
+                -- if inside of pillar
                 oldVY = players[i].vY
                 players[i].vY = 0
                 if not players[i].touchingGround then
@@ -111,22 +131,33 @@ function love.update(dt)
                         players[i].vY = -jumpForce
                     end
                 end
+                
                 if players[i].y + size > love.graphics.getHeight() - groundHeight - pillars[pillarIndex] then
+                    -- if outside of screen
+
                     -- players[i].vX= -players[i].vX
                     oldY = players[i].y
                     players[i].y = love.graphics.getHeight() - groundHeight - pillars[pillarIndex] -size
                     players[i].vY = players[i].vY - (players[i].y - oldVY) * launchMul
                 elseif not players[i].touchingGround then
+                    -- if landed this frame
+
                     players[i].touchingGround = true
                     debugval = oldVY
                     if oldVY > 150 and math.abs(players[i].vX)/players[i].vY > 1 then
+                        -- smash pillar below if hitting ground from sharp angle
+                        -- causes pillar on opposite side of screen to rise depending on torque and velocity transferred
                         pillars[pillarCount- pillarIndex+1] = pillars[pillarCount - pillarIndex+1] + math.abs(players[i].torque) * torqueToHeight + oldVY * velocityToHeight
                         players[i].torque = 0 
                     end
+
+                    -- snap player to ground/pillar surface
                     players[i].y = love.graphics.getHeight() - groundHeight - size - pillars[pillarIndex]
                 end
+                
             end
         else
+            -- bounce off ceiling
             players[i].vY = -players[i].vY
         end
         
@@ -140,7 +171,7 @@ function love.update(dt)
                 players[i].y = newPillarHeight - size
             end
         else
-            players[i].vX = -players[i].vX
+            players[i].vX = -players[i].vX * turnLoss
             if not players[i].touchingGround and players[i].yDir < 0 then
                 players[i].vY = -jumpForce
             end
